778019b4ce989b4791c70f3ae2ae0af87dd5b22f
Erica Andrew <erica.nicole.andrew@gmail.com>
2018-02-15 22:17:33 -0700

    stuff

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,5 @@
+@include('partials.start')
+
+    @yield('content')
+
+@include('partials.end')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.home')
 
 @section('content')
     <div class="welcome-wrapper">
+         @include('partials.home-nav')
+
         <div class="container">
             <div class="row">
-                <h1 class="text-center">Erica Andrew</h1>
-            </div>
-
-            <div class="row">
-                <img class="center-block" src="/img/layout/small_scarab.png" />
+                
             </div>
         </div>  
     </div>
